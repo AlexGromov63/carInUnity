f46ad79b13e033832a0f75401bdbe4102efa4c65
Your Name <you@example.com>
2023-01-31 20:26:35 +0300

    init

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float speed = 20.0f;
+    public float turnspeed = 10.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        var HorizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnspeed * HorizontalInput);
+    }
+}
